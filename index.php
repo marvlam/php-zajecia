4b9c1650e5b852c7e1edccb63c6bcea82ff75b03
Maryla Mielniczuk <4359683@teb-gdansk.ad>
2023-03-25 11:32:58 +0100

    Improvements

--- index.php
+++ index.php
@@ -13,10 +13,20 @@ require_once
 
 // $_GET - zapytanie serwera
 // $_POST - wysłanie do serwera
+
 include_once('./src/View.php');
 include_once('./src/utils/debug.php');
 
+const DEFAULT_ACTION ='list';
+$action = $_GET['action'] ?? DEFAULT_ACTION;
 
-$action = $_GET['action'] ?? null;
+$viewParams = [];
+if($action === 'create') {
+    $page = 'create';
+    $viewParams['resultCreate'] = 'Udało się dodać notatkę!';
+} else {
+    $page = 'list';
+    $viewParams['resultList'] = 'Wyświetlono listę notatek!';
+}
 $view = new View();
-$view->render($action);
+$view->render($page, $viewParams);
--- src/View.php
+++ src/View.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App;
 
 class View {
-    public function render(?string $page):void {
+    public function render(string $page, array $params):void {
         include_once('./templates/layout.php');
     }
 }
